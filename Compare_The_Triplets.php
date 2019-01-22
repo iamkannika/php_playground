a2cd4e90948f6eb19086b9cd07325efd74fbe7d0
Kannika <>
2019-01-22 12:05:24 +0700

    update_compare the triplets

---
+++ Compare_The_Triplets.php
@@ -0,0 +1,33 @@
+<?php
+
+// Complete the compareTriplets function below.
+function compareTriplets($a, $b) {
+    $sum_a =0;
+    $sum_b =0;
+    for($i=0;$i<3;$i++){
+        if($a[$i]>$b[$i]) {
+            $sum_a = $sum_a+1;
+        }
+        if($a[$i]<$b[$i]) {
+            $sum_b = $sum_b+1;
+        }
+    }
+
+     return [$sum_a, $sum_b];
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$a_temp = rtrim(fgets(STDIN));
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$b_temp = rtrim(fgets(STDIN));
+
+$b = array_map('intval', preg_split('/ /', $b_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = compareTriplets($a, $b);
+
+fwrite($fptr, implode(" ", $result) . "\n");
+
+fclose($fptr);
